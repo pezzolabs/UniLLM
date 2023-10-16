054f2c7d7370f3c10d6e8aa5faef58d52b84a491
Ariel Weinberger <weinberger.ariel@gmail.com>
2023-10-15 19:04:22 -0700

    chore: fix build

---
+++ packages/uni-llm-node/rollup.config.js
@@ -0,0 +1,39 @@
+
+const path = require("path");
+const commonjs = require("@rollup/plugin-commonjs");
+const typescript2 = require("rollup-plugin-typescript2");
+const copy = require("rollup-plugin-copy");
+
+const { defineConfig } = require('rollup');
+
+module.exports = defineConfig({
+  input: "./index.ts",
+  output: {
+    dir: "./dist",
+    sourcemap: true,
+  },
+  plugins: [
+    typescript2({
+      tsconfig: path.resolve(__dirname, "tsconfig.json"),
+      compilerOptions: { outDir: "./dist", sourceMap: true },
+      useTsconfigDeclarationDir: true,
+    }),
+    commonjs(),
+    copy({
+      targets: [
+        {
+          src: path.resolve(__dirname, "package.json"),
+          dest: "./dist"
+        },
+        {
+          src: path.resolve(__dirname, "../../", "README.md"),
+          dest: "./dist"
+        },
+        {
+          src: path.resolve(__dirname, "../../", "LICENSE"),
+          dest: "./dist"
+        }
+      ]
+    }),
+  ]
+});
